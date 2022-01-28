ba152c370f5e8dade70d293a2f1e2ac5b40f2dca
Haqi Ramadhani <hakiramadhani@gmail.com>
2022-01-28 14:05:07 +0700

    Not Working Well

---
+++ src/app.service.ts
@@ -0,0 +1,35 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { CreateUserRequestDto } from './create-user-request.dto';
+import { CreateUserEvent } from './create-user.event';
+
+@Injectable()
+export class AppService {
+  private readonly users: any[] = [];
+
+  constructor(
+    @Inject('COMMUNICATION') private readonly communicationClient: ClientProxy,
+    @Inject('ANALYTICS') private readonly analyticsClient: ClientProxy,
+  ) {}
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  createUser(createUserRequest: CreateUserRequestDto) {
+    this.users.push(createUserRequest);
+    this.communicationClient.emit(
+      'user_created',
+      new CreateUserEvent(createUserRequest.email),
+    );
+    this.analyticsClient.emit(
+      'user_created',
+      new CreateUserEvent(createUserRequest.email),
+    );
+  }
+
+  getAnalytics() {
+    console.log('getAnalytics - ANALYTICS');
+    return this.analyticsClient.send({ cmd: 'get_analytics' }, {});
+  }
+}
